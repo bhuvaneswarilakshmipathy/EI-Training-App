2a5cc2005ba07d319022435570732e22033b4518
bhuvaneswarilakshmipathy <128119134+bhuvaneswarilakshmipathy@users.noreply.github.com>
2026-08-08 14:46:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const moduleRoutes = require("./routes/moduleRoutes");
 app.use("/api/modules", moduleRoutes);
   
 //MongoDB Connection
-mongoose.connect("mongodb+srv://bharesearchcon_db_user:RYUMCYC4t32kKmeo@cluster0.i5ickhw.mongodb.net/serve?retryWrites=true&w=majority") 
+mongoose.connect("mongodb+srv://username:password@cluster0.i5ickhw.mongodb.net/serve?retryWrites=true&w=majority") 
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("DB Error:", err));
 app.listen(5000, () => {
